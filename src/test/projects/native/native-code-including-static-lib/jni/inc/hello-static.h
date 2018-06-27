32024e5bef773566634d0dd73051fa02942bc03f
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2018-06-27 18:59:46 +0300

    Refactoring

--- src/test/projects/native/native-code-including-static-lib/jni/inc/hello-static.h
+++ src/test/projects/native/native-code-including-static-lib/jni/inc/hello-static.h
@@ -15,7 +15,6 @@
  *
  */
 #include <string.h>
-#include <jni.h>
 
 /* This is a trivial JNI example where we use a native method
  * to return a new VM String. See the corresponding Java source
@@ -23,9 +22,4 @@
  *
  *   apps/samples/hello-jni/project/src/com/example/HelloJni/HelloJni.java
  */
-jstring
-Java_com_acme_hellojni_HelloJni_stringFromJNI( JNIEnv* env,
-                                                  jobject thiz )
-{
-    return (*env)->NewStringUTF(env, string_from_static());
-}
+const char* string_from_static();
